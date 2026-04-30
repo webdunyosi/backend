62de8072de950c9c1543b713f303730ca3b90a04
webdunyosi <alimardontoshpulatov1978@gmail.com>
2026-04-30 09:07:56 +0500

    feat: add nodemon for development and update app.js routes

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-import express from 'express'
+import express from "express"
 
 const app = express()
+
+app.get("/", (req, res) => {
+  res.status(200).json({message: "Salom Webdunyosi!"})
+})
+
+app.get("/post", (req, res) => {
+  res.status(200).json({message: "Bu post sahifasi"})
+})
+
 const PORT = 8080
 
 app.listen(PORT, () => {
-    console.log(`Listening on - http://localhost:${PORT}`)
+  console.log(`Listening on - http://localhost:${PORT}`)
 })
